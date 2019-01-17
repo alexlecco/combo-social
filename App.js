110cbab18fe8d54877b939c38c0448c7a84dee5e
alex.villecco <alex.villecco@globant.com>
2019-01-16 23:43:59 -0300

    create static list of restaurants

--- App.js
+++ App.js
@@ -1,11 +1,56 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
+import { Header, Card, Button } from 'react-native-elements';
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+      <View>
+        <Header
+          centerComponent={{ text: '¿Donde querés comer?', style: { color: '#ffffff' } }}
+        />
+
+        <ScrollView>
+          <Card
+            title='A mi no'
+            image={require('./assets/bar1.png')}>
+            <Text style={{marginBottom: 10}}>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae sequi et nesciunt.
+            </Text>
+            <Button
+              icon={{name: 'code'}}
+              backgroundColor='#03A9F4'
+              buttonStyle={{borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0}}
+              title='comer aquí' />
+          </Card>
+
+          <Card
+            title='La pizzada'
+            image={require('./assets/bar2.png')}>
+            <Text style={{marginBottom: 10}}>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae sequi et nesciunt.
+            </Text>
+            <Button
+              icon={{name: 'code'}}
+              backgroundColor='#03A9F4'
+              buttonStyle={{borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0}}
+              title='comer aquí' />
+          </Card>
+
+          <Card
+            title='Tutti'
+            image={require('./assets/bar3.png')}>
+            <Text style={{marginBottom: 10}}>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae sequi et nesciunt.
+            </Text>
+            <Button
+              icon={{name: 'code'}}
+              backgroundColor='#03A9F4'
+              buttonStyle={{borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0}}
+              title='comer aquí' />
+          </Card>
+        </ScrollView>
+
       </View>
     );
   }
@@ -14,7 +59,7 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ffffff',
     alignItems: 'center',
     justifyContent: 'center',
   },
