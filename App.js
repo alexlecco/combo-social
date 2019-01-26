e17b319862a3c4cb2a1e427d25eb9a2d7027d521
alex.villecco <alex.villecco@globant.com>
2019-01-26 17:45:29 -0300

    Add Firebase connection in App.js

--- App.js
+++ App.js
@@ -1,56 +1,78 @@
-import React from 'react';
-import { StyleSheet, Text, View, ScrollView } from 'react-native';
-import { Header, Card, Button } from 'react-native-elements';
+import React from 'react'
+import { StyleSheet, View, ScrollView, ListView, YellowBox } from 'react-native'
+import { Header, Card, Button } from 'react-native-elements'
+
+import { firebaseApp } from './firebase'
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      dataSourceRestaurants: new ListView.DataSource({ rowHasChanged: (row1, row2) => row1 !== row2 }),
+    };
+
+    this.restaurantsRef = firebaseApp.database().ref().child('restaurants')
+
+    console.disableYellowBox = true;
+    console.warn('YellowBox is disabled.');
+    YellowBox.ignoreWarnings(['Warning: ...']);
+    console.ignoredYellowBox = ['Setting a timer'];
+  }
+
+  componentDidMount() {
+    this.listenForRestaurants(this.restaurantsRef)
+  }
+
+  listenForRestaurants(restaurantsRef) {
+    restaurantsRef.on('value', (snap) => {
+      let restaurants = [];
+      snap.forEach((child) => {
+        restaurants.push({
+          addres: child.val().addres,
+          id: child.val().id,
+          name: child.val().name,
+          _key: child.key,
+        });
+      });
+      this.setState({
+        dataSourceRestaurants: this.state.dataSourceRestaurants.cloneWithRows(restaurants)
+      })
+    });
+  }
+
+  getImage(id) {
+    return `https://firebasestorage.googleapis.com/v0/b/combo-social.appspot.com/o/restaurants%2F${id}.png?alt=media&token=84f5ccd9-506d-4940-a318-703169c32b60`
+  }
+
+  buildRestaurant(restaurant) {
+    return(
+      <Card
+        title={restaurant.name}
+        image={{uri: this.getImage(restaurant.id)}} >
+        <Button
+          icon={{name: 'check'}}
+          backgroundColor='#03A9F4'
+          buttonStyle={{borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0}}
+          title='comer aquí' />
+      </Card>
+    );
+  }
+
   render() {
     return (
       <View>
-        <Header
-          centerComponent={{ text: '¿Donde querés comer?', style: { color: '#ffffff' } }}
-        />
-
         <ScrollView>
-          <Card
-            title='A mi no'
-            image={require('./assets/bar1.png')}>
-            <Text style={{marginBottom: 10}}>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae sequi et nesciunt.
-            </Text>
-            <Button
-              icon={{name: 'code'}}
-              backgroundColor='#03A9F4'
-              buttonStyle={{borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0}}
-              title='comer aquí' />
-          </Card>
+          <Header
+            centerComponent={{ text: '¿Donde querés comer?', style: { color: '#ffffff' } }}
+          />
 
-          <Card
-            title='La pizzada'
-            image={require('./assets/bar2.png')}>
-            <Text style={{marginBottom: 10}}>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae sequi et nesciunt.
-            </Text>
-            <Button
-              icon={{name: 'code'}}
-              backgroundColor='#03A9F4'
-              buttonStyle={{borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0}}
-              title='comer aquí' />
-          </Card>
+          <ListView
+            dataSource={this.state.dataSourceRestaurants}
+            renderRow={(restaurant) => this.buildRestaurant(restaurant)}
+          />
 
-          <Card
-            title='Tutti'
-            image={require('./assets/bar3.png')}>
-            <Text style={{marginBottom: 10}}>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae sequi et nesciunt.
-            </Text>
-            <Button
-              icon={{name: 'code'}}
-              backgroundColor='#03A9F4'
-              buttonStyle={{borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0}}
-              title='comer aquí' />
-          </Card>
         </ScrollView>
-
       </View>
     );
   }
---
+++ firebase.js
@@ -0,0 +1,18 @@
+import * as firebase from 'firebase';
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyDEriYiHfdPb0TKWlGBkRj66XMh_RkFHu4",
+  authDomain: "combo-social.firebaseapp.com",
+  databaseURL: "https://combo-social.firebaseio.com",
+  projectId: "combo-social",
+  storageBucket: "combo-social.appspot.com",
+  messagingSenderId: "306137843491"
+};
+firebase.initializeApp(config);
+
+const firebaseApp = firebase;
+
+export {
+	firebaseApp,
+};
