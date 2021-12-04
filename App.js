7d518dcd04c091f9336e6f9fd5e5cd7484c86f05
Alex Villecco <amvillecco@teco.com.ar>
2021-12-04 10:59:57 -0300

    reduce console warnigns

--- App.js
+++ App.js
@@ -1,27 +1,23 @@
+import React from 'react';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { StatusBar } from 'expo-status-bar';
-import React, { useEffect } from 'react';
-import { StyleSheet, View, LogBox } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 
 import ContextProvider from './context/provider';
 import ComboApp from './ComboApp';
 
 
 const App = _ => {
-  useEffect(() => {
-    // Ignore log notification by message:
-    LogBox.ignoreLogs(['Warning: ...']);
-  
-    // Ignore all log notifications:
-    LogBox.ignoreAllLogs();
-  })
 
   return(
-    <View style={styles.container}>
-      <ContextProvider>
-        <ComboApp />
-      </ContextProvider>
-      <StatusBar style='auto' />
-    </View>
+    <SafeAreaProvider>
+      <View style={styles.container}>
+        <ContextProvider>
+          <ComboApp />
+        </ContextProvider>
+        <StatusBar style='auto' />
+      </View>
+    </SafeAreaProvider>
   );
 }
 
--- firebase.js
+++ firebase.js
@@ -1,4 +1,5 @@
-import * as firebase from 'firebase';
+import firebase from 'firebase/app';
+import 'firebase/firebase';
 
 var config = {
   apiKey: "AIzaSyDEriYiHfdPb0TKWlGBkRj66XMh_RkFHu4",
