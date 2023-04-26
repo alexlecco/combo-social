dc4daf1245fafcbe5fa18052b967ea2fd3946e9e
Alex Villecco <amvillecco@teco.com.ar>
2023-04-26 20:20:37 -0300

    add status constant

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,9 +1,11 @@
 import colors from './colors';
 import bgImages from './bgImages';
 import rolls from './rolls';
+import status from './status';
 
 export {
     colors,
     bgImages,
     rolls,
+    status,
 };
---
+++ src/constants/status.js
@@ -0,0 +1,24 @@
+const orderStatus = [{
+  PENDING: {
+    status: 'pending',
+    text: 'pendiente',
+    color: 'red',
+  }}, {
+  ACCEPTED: {
+    status: 'accepted',
+    text: 'preparando',
+    color: 'orange',
+  }}, {
+  DELIVERED: {
+    status: 'delivered',
+    text: 'entregado',
+    color: 'green',
+  }}, {
+  PAYED: {
+    status: 'payed',
+    text: 'pagado',
+    color: 'blue',
+  }},
+];
+
+export default orderStatus;
